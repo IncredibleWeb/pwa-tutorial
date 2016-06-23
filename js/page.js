1f6abc15750e75c9f01b9338a14fdc1580f6e196
Kevin Farrugia <kevin.farrugia.87@gmail.com>
2016-06-23 18:24:54 +0200

    Added service worker and caching of App Shell

--- js/page.js
+++ js/page.js
@@ -17,4 +17,13 @@
         FastClick.attach(document.body);
     });
 
+    // register the service worker if available
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js').then(function(reg) {
+            console.log('Successfully registered service worker', reg);
+        }).catch(function(err) {
+            console.warn('Error whilst registering service worker', err);
+        });
+    }
+
 })(jQuery);
