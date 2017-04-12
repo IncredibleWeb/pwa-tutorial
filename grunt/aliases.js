d2815bc3864af94f9fde408eee4075cfa9bf9af9
Ross Olson <Ross@rossolson.com>
2017-04-12 15:44:42 -0700

    Update aliases.js
    Default `start` script added

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,4 +1,11 @@
 ﻿module.exports = function (grunt) {
     grunt.registerTask('dev', ['uglify:dev']);
     grunt.registerTask('dev_watch', ['uglify:dev', 'watch']);
+    grunt.registerTask('start', function () {
+        grunt.util.spawn({
+            cmd: 'node', args: ['app.js']
+        })
+        grunt.task.run('watch')
+        console.log('Server started at: http://localhost:3000');
+    })
 };
