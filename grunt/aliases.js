dccd666d612dbc8d2c0ee93f4aa947c2b1d933c1
Ross Olson <Ross@rossolson.com>
2017-04-13 10:26:41 -0700

    Update aliases.js
    Moving the default task *back* in to the Grunt config.

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,4 +1,12 @@
 ﻿module.exports = function (grunt) {
     grunt.registerTask('dev', ['uglify:dev']);
     grunt.registerTask('dev_watch', ['uglify:dev', 'watch']);
+    grunt.registerTask('default', function () {
+        grunt.util.spawn({
+            cmd: 'node', args: ['app.js']
+        })
+        grunt.task.run('watch')
+        console.log('');
+        console.log('>>> Server started at: http://localhost:3000/ <<<');
+    })
 };
